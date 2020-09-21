1c87c0ad601c8bfb974923ffa4e985dc3dbc2988
ekke <63651605+victortemnov@users.noreply.github.com>
2020-09-21 10:37:38 +0300

    Update parse-xml-json.js

--- C5.2/parse-xml-json.js
+++ C5.2/parse-xml-json.js
@@ -43,31 +43,32 @@ console.log(result);
 
 // JSON
 
-const jsonDATA = `
+const jsonString = `
 {
- "list": [
-  {
-   "name": "Petr",
-   "age": "20",
-   "prof": "mechanic"
-  },
-  {
-   "name": "Vova",
-   "age": "60",
-   "prof": "pilot"
-  }
+ "list": 
+ [
+              {
+                   "name": "Petr",
+                   "age": "20",
+                   "prof": "mechanic"
+              },
+              { 
+                  "name": "Vova",
+                  "age": "60",
+                  "prof": "pilot"
+              }
  ]
 }`;
 
-const data = JSON.parse(jsonDATA);
+const data = JSON.parse(jsonString);
 const list = data.list;
 const first = list[0];
 const second = list[1];
 const result =
-{
-    list: [
-        { name: first.name, age: Number(first.age), prof: first.prof },
-        { name: second.name, age: Number(second.age), prof: second.prof },
-    ]
-}
-console.log(result)
+    {
+        list: [
+            { name: first.name, age: Number(first.age), prof: first.prof },
+            { name: second.name, age: Number(second.age), prof: second.prof },
+        ]
+    }
+console.log(result);
